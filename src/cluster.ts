9a62201da91185c7afb89bcb219e41aedf480ce5
kolirina <irinairin@list.ru>
2024-10-21 17:04:56 +0300

    fix: fix clusters bugs

--- src/cluster.ts
+++ src/cluster.ts
@@ -7,57 +7,96 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const numCPUs = os.cpus().length;
-
 const PORT = Number(process.env.PORT) || 3000;
 
-if (cluster.isPrimary) {
-  let currentWorker = 0;
+let userDB: { [key: string]: any } = {};
 
+interface CreateUserMessage {
+  type: "createUser";
+  data: any;
+}
+
+interface GetUserMessage {
+  type: "getUser";
+  userId: string;
+}
+
+interface UserCreatedMessage {
+  type: "userCreated";
+  userId: string;
+}
+
+interface SendUserMessage {
+  type: "sendUser";
+  user: any;
+}
+
+type Message =
+  | CreateUserMessage
+  | GetUserMessage
+  | UserCreatedMessage
+  | SendUserMessage;
+
+if (cluster.isPrimary) {
   console.log(`Primary process is running on port ${PORT}`);
 
-  const loadBalancer = http.createServer((req, res) => {
-    currentWorker = (currentWorker + 1) % numCPUs;
-    const workerPort = PORT + 1 + currentWorker;
-
-    const options = {
-      hostname: "localhost",
-      port: workerPort,
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    };
-
-    const proxy = http.request(options, (proxyRes) => {
-      res.writeHead(proxyRes.statusCode!, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
-
-    req.pipe(proxy, { end: true });
-  });
-
-  loadBalancer.listen(PORT, () => {
-    console.log(`Load balancer is running on port ${PORT}`);
+  cluster.on("message", (worker, message: Message) => {
+    if (message.type === "createUser") {
+      const user = message.data;
+      const userId = user.id;
+      userDB[userId] = user;
+      worker.send({ type: "userCreated", userId });
+    } else if (message.type === "getUser") {
+      const user = userDB[message.userId];
+      worker.send({ type: "sendUser", user });
+    }
   });
 
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({ WORKER_PORT: PORT + 1 + i });
-  }
-
-  cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} died, restarting...`);
     cluster.fork();
-  });
+  }
 } else {
-  const workerPort = process.env.WORKER_PORT;
   const server = createServer();
 
   server.on("request", (req, res) => {
-    console.log(`Worker ${process.pid} handling request for ${req.url}`);
+    if (req.method === "POST" && req.url === "/users") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", () => {
+        const userData = JSON.parse(body);
+        const message: CreateUserMessage = {
+          type: "createUser",
+          data: userData,
+        };
+        process.send!(message);
+        process.on("message", (message: Message) => {
+          if (message.type === "userCreated") {
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ userId: message.userId }));
+          }
+        });
+      });
+    } else if (req.method === "GET" && req.url!.startsWith("/users/")) {
+      const userId = req.url!.split("/")[2];
+      const message: GetUserMessage = { type: "getUser", userId };
+      process.send!(message);
+      process.on("message", (message: Message) => {
+        if (message.type === "sendUser") {
+          if (message.user) {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message.user));
+          } else {
+            res.writeHead(404);
+            res.end("User not found");
+          }
+        }
+      });
+    }
   });
 
-  server.listen(workerPort, () => {
-    console.log(
-      `Worker process ${process.pid} is listening on port ${workerPort}`
-    );
+  server.listen(PORT, () => {
+    console.log(`Worker process ${process.pid} is listening on port ${PORT}`);
   });
 }
